a050f20d8297acadc5699c361c1c1ad73ed682ec
janemrivera <jane.m.rivera@gmail.com>
2017-02-15 08:20:34 -0600

    added modules

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var apiai = require('apiai');
+var apiaiApp = apiai('4dc8d03e753c4a6db3907dd67380dcfe');
 //Specify a port
 var port = process.env.port || 8080;
 
